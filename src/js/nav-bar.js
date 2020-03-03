00d163d5fb250db1869b23ce6fb25482617053cc
jbis9051 <josh9051@gmail.com>
2020-03-03 13:07:45 -0500

    fix nav bar

--- src/js/nav-bar.js
+++ src/js/nav-bar.js
@@ -1,3 +1,6 @@
+const header = document.querySelector('.header');
+const hamburger = document.querySelector('.hamburger-wrapper');
+
 hamburger.addEventListener("click", function(e) {
     header.classList.toggle("active");
 });
