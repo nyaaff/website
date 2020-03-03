334c278dbaf564198a4ff51609d8cf7eb5a6bc7a
jbis9051 <josh9051@gmail.com>
2020-03-03 12:56:23 -0500

    Makes requested changes from email 3/2/2020 4:45pm.

--- getData.js
+++ getData.js
@@ -1,19 +1,43 @@
+const moment = require('moment');
+
 module.exports = {
     './efforts.ejs': {
-        efforts: require('./data/positions').sort(((a, b) => a.title.localeCompare(b.title)))
+        efforts: require('./data/positions')
+            .map(effort => {
+                effort.date = moment(effort.date, 'MM-DD-YYYY').toDate();
+                effort.files = effort.files
+                    .map(file => {
+                        return {
+                            name: file,
+                            date: moment(file.match(/(\d+\.\d+\.\d+)-/)[1], 'YYYY-MM-DD').toDate()
+                        }
+                    })
+                    .sort((a, b) => {
+                        return b.date - a.date;
+                    });
+                return effort;
+            })
+            .sort((a, b) => {
+                return b.date - a.date;
+            })
     },
     './members.ejs': {
         members: (() => {
-            const members = require('./data/members').sort(((a, b) => a.name.localeCompare(b.name)));
+            const members = require('./data/members');
+            Object.keys(members).slice(1).forEach(key => {
+                members[key] = members[key].sort(((a, b) => a.name.localeCompare(b.name)));
+            });
+
             const pics = require('./data/profile-pic');
-            members.forEach(member => { // SELECT * FROM members INNER JOIN  "profile-pic" ON members.id = "profile-pic".id
+            Object.values(members).forEach(arr => arr.forEach(member => { // SELECT * FROM members INNER JOIN  "profile-pic" ON members.id = "profile-pic".id
                 if (pics[member.id]) {
                     member.image = pics[member.id];
                     return;
                 }
                 member.image = null;
-            });
+            }));
             return members;
         })()
     }
 };
+console.log()
